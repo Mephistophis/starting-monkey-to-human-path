a58cc59a41ef9719d8f7863ca92354a468f1bcb7
Mephistophis <mephistophis1@gmail.com>
2019-09-17 21:56:48 +0400

    Added entry point

---
+++ src/RPIS71/Tsvikatskiy/wdad/Application.java
@@ -0,0 +1,7 @@
+package RPIS71.Tsvikatskiy.wdad;
+
+public class Application {
+    public static void main(String[] args){
+        System.out.println("I'm Jotaro Kujo, and I'm not a monkey");
+    }
+}
